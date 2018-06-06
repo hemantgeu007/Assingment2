ffc85884c10057348966851a76ead1e3b1d1aa74
Hemant Singh Pawnar <31535586+hemantgeu007@users.noreply.github.com>
2018-06-06 09:50:46 +0530

    Add files via upload

---
+++ Assingment2.py
@@ -0,0 +1,27 @@
+'''*Q1*'''
+print("My name is Hemant")
+
+'''Q2'''
+print("Acad" + "View")
+
+'''Q3'''
+x = input('x = ')
+y = input('y = ')
+z = input('z = ')
+print('x = ' + x)
+print('y = ' + y)
+print('z = ' + z)
+
+'''Q4'''
+print("Let's get started")
+
+'''Q5'''
+s = 'Acadview'
+course = 'Python'
+fees = 5000
+print("In {}, {} course is for Rs {}".format(s, course, fees))
+
+'''Q6'''
+name = "Tony Stark"
+salary = 1000000
+print('%s %d ' %(name,salary))
